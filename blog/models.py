6449a33645b594e2d74ebbb72c5d0cef1804bfbc
Shynggys Zharas <sm.zharas@gmail.com>
2020-06-17 22:42:32 +0600

    posts added to admin page

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_poster = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 14, 2020'
-    },
-    {
-        'author': 'Shynggys Zharas',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 15, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
