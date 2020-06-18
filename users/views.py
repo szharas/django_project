6995be7595e538c02c04ed2717b2ac874fb946d6
Shynggys Zharas <sm.zharas@gmail.com>
2020-06-18 23:48:19 +0600

    user forms added: login, logout, profile

--- users/views.py
+++ users/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for {}!'.format(username))
-            return redirect('blog-home')
+            messages.success(request, 'Your account has been created. You can now log in!')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
